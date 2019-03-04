8267f70a0bc8707165845e69ee7f494afc34b6a4
Truong <checkraiser11@gmail.com>
2019-03-05 04:15:30 +0700

    Fix ts

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,15 @@
 'use strict';
+const path = require('path');
 
 module.exports = {
+  modify: (config, { target, dev }, webpack) => {
+    const appConfig = Object.assign({}, config)
+    appConfig.resolve.alias = {
+      ...appConfig.resolve.alias,
+      components: path.join(__dirname, "src", "components")
+    }
+
+    return appConfig;
+  },
   plugins: ['typescript'],
 };
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Test from 'components/Test';
+
 const Home = () => {
   return (
     <div>
       Hello world 1
+      <Test />
     </div>
   );
 };
